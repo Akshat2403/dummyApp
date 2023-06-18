cf8796ddd541601cb07bff68c7c73d1386eff594
Akshat2403 <95174507+Akshat2403@users.noreply.github.com>
2023-06-18 10:14:58 +0530

    req

--- dummyApp/urls.py
+++ dummyApp/urls.py
@@ -19,7 +19,7 @@ from django.contrib import admin
 from django.urls import path, re_path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', admin.site.urls),
     
 ]
 if settings.DEBUG:
